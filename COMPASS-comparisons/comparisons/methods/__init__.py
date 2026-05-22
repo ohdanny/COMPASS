a2a9ae960759700f66943d3887146ac18f7939e2
Hyoung-in Oh <hyounginoh@gmail.com>
2026-05-22 12:33:06 -0400

    Delete COMPASS-comparisons directory

--- COMPASS-comparisons/comparisons/methods/__init__.py
+++
@@ -1,5 +0,0 @@
-from .compass_method import CompassMethod
-from .spvc_method import SPVCMethod
-from .splisosm_method import SplisosmMethod
-
-__all__ = ["CompassMethod", "SPVCMethod", "SplisosmMethod"]
